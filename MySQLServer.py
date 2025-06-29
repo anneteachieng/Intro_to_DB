23de846586dd80ced85b7738cf9e16feb55a89c1
Annete <achiengomondi02@gmail.com>
2025-06-29 22:04:56 +0300

    server

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     try:
@@ -14,7 +13,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error: {e}")
 
     finally:
@@ -25,4 +24,3 @@ def create_database():
 
 if __name__ == "__main__":
     create_database()
-
